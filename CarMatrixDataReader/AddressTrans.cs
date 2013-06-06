f04ceb048b6ed3c583b5148ef0095e47d310f5e8
fanfei <shfanfei@live.cn>
2013-06-06 22:39:26 +0800

    address to location
    add trans address to location by baidu map api

--- CarMatrixDataReader/AddressTrans.cs
+++ CarMatrixDataReader/AddressTrans.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,34 @@ namespace CarMatrixDataReader
 {
     public class AddressTrans
     {
+        private HttpWebRequest request;
+        private static readonly string UserAgent = "Mozilla/4.5";
+
         public void TransAddress()
-        { 
-            //TO DO
+        {
+
+        }
+
+        public string HttpRequest(string url)
+        {
+            string content = "";
+            try
+            {
+                this.request = HttpWebRequest.Create(url) as HttpWebRequest;
+                this.request.Method = "GET";
+                this.request.UserAgent = UserAgent;
+
+                Stream stream = this.request.GetResponse().GetResponseStream();
+                using (StreamReader sr = new StreamReader(stream, Encoding.UTF8))
+                {
+                    content = sr.ReadToEnd();
+                }
+            }
+            catch
+            {
+
+            }
+            return content;
         }
     }
 }
--- CarMatrixDataReader/Program.cs
+++ CarMatrixDataReader/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,22 @@ namespace CarMatrixDataReader
     {
         static void Main(string[] args)
         {
-            using (var container = new ModelsContainer())
-            {
-                Person p = new Person();
-                p.Name = "jack";
+            //using (var container = new ModelsContainer())
+            //{
+            //    Person p = new Person();
+            //    p.Name = "jack";
 
-                container.Set<Person>().Add(p);
-                container.SaveChanges();
-            }
+            //    container.Set<Person>().Add(p);
+            //    container.SaveChanges();
+            //}
+
+            string mapKey = ConfigurationManager.AppSettings["map_key"];
+            string url = "http://api.map.baidu.com/geocoder/v2/" + "?address=百度大厦&output=xml&ak=" + mapKey;
+
+            AddressTrans at = new AddressTrans();
+            string content = at.HttpRequest(url);
+            Console.WriteLine("{0}", content);
+            Console.ReadKey();
 
             Console.ReadKey();
         }
