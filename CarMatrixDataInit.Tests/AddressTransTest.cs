4033245341ca8e481cd2d91d0cdb6ecc2bda586b
fanfei <shfanfei@live.cn>
2013-06-06 23:34:05 +0800

    add data init unit test project
    add data init unit test project,and add some AddressTrans class code

---
+++ CarMatrixDataInit.Tests/AddressTransTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CarMatrixDataReader;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarMatrixDataInit.Tests
+{
+    [TestClass]
+    public class AddressTransTest
+    {
+        [TestMethod]
+        public void TestBuildeUrl()
+        {
+            AddressTrans at = new AddressTrans();
+            string url = at.BuildeUrl("百度大厦");
+            //Assert.AreEqual("", url);
+        }
+    }
+}
--- CarMatrixDataReader/AddressTrans.cs
+++ CarMatrixDataReader/AddressTrans.cs
@@ -1,21 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using CarMatrixData.Models;
 
 namespace CarMatrixDataReader
 {
     public class AddressTrans
     {
         private HttpWebRequest request;
+        private string mapKey;
         private static readonly string UserAgent = "Mozilla/4.5";
+        //private static readonly string server = "http://api.map.baidu.com/geocoder/v2/";
 
         public void TransAddress()
         {
+            mapKey = ConfigurationManager.AppSettings["map_key"];
+        }
 
+        public void GetPersonAddressLocation(IEnumerable<Person> persons)
+        {
+            if (persons != null)
+            {
+                foreach (Person person in persons)
+                {
+                    if (!string.IsNullOrEmpty(person.Address))
+                    {
+                        string url = BuildeUrl(person.Address);
+                        string content = HttpRequest(url);
+                        if (!string.IsNullOrEmpty(content))
+                        { 
+                            // TO DO 
+                        }
+                    }
+                }
+            }
+            else
+                Console.WriteLine("{0}", "person params is null");
+        }
+
+        public string BuildeUrl(string address)
+        {
+            return string.Format("http://api.map.baidu.com/geocoder/v2/?address={0}&output=xml&ak={1}", address, mapKey); ;
         }
 
         public string HttpRequest(string url)
@@ -33,11 +64,18 @@ namespace CarMatrixDataReader
                     content = sr.ReadToEnd();
                 }
             }
-            catch
+            catch (Exception ex)
             {
-
+                Console.WriteLine("get request has exception,{0}", ex);
             }
             return content;
+        }
+
+        public void ResolvePersonLocation(string content)
+        {
+            XmlDocument document = new XmlDocument();
+            document.LoadXml(content);
+
         }
     }
 }
