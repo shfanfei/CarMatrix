154c8b95448d8d6c87ef4f273ff437d7f6e4d112
fanfei <shfanfei@live.cn>
2013-06-08 09:38:25 +0800

    add xml content resolve
    add xml content resolve

--- CarMatrixDataReader/AddressTrans.cs
+++ CarMatrixDataReader/AddressTrans.cs
@@ -4,9 +4,11 @@ using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Linq;
 using CarMatrixData.Models;
 
 namespace CarMatrixDataReader
@@ -23,7 +25,7 @@ namespace CarMatrixDataReader
             mapKey = ConfigurationManager.AppSettings["map_key"];
         }
 
-        public void GetPersonAddressLocation(IEnumerable<Person> persons)
+        public async void GetPersonAddressLocation(IEnumerable<Person> persons)
         {
             if (persons != null)
             {
@@ -32,10 +34,11 @@ namespace CarMatrixDataReader
                     if (!string.IsNullOrEmpty(person.Address))
                     {
                         string url = BuildeUrl(person.Address);
-                        string content = HttpRequest(url);
+                        string content = await Taskmethod(url);
                         if (!string.IsNullOrEmpty(content))
-                        { 
+                        {
                             // TO DO 
+
                         }
                     }
                 }
@@ -49,33 +52,70 @@ namespace CarMatrixDataReader
             return string.Format("http://api.map.baidu.com/geocoder/v2/?address={0}&output=xml&ak={1}", address, mapKey); ;
         }
 
-        public string HttpRequest(string url)
-        {
-            string content = "";
-            try
-            {
-                this.request = HttpWebRequest.Create(url) as HttpWebRequest;
-                this.request.Method = "GET";
-                this.request.UserAgent = UserAgent;
+        //public string HttpRequest(string url)
+        //{
+        //    string content = "";
+        //    try
+        //    {
+        //        this.request = HttpWebRequest.Create(url) as HttpWebRequest;
+        //        this.request.Method = "GET";
+        //        this.request.UserAgent = UserAgent;
 
-                Stream stream = this.request.GetResponse().GetResponseStream();
-                using (StreamReader sr = new StreamReader(stream, Encoding.UTF8))
-                {
-                    content = sr.ReadToEnd();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("get request has exception,{0}", ex);
-            }
+        //        Stream stream = this.request.GetResponse().GetResponseStream();
+        //        using (StreamReader sr = new StreamReader(stream, Encoding.UTF8))
+        //        {
+        //            content = sr.ReadToEnd();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("get request has exception,{0}", ex);
+        //    }
+        //    return content;
+        //}
+
+
+
+        public async Task<string> Taskmethod(string url)
+        {
+            var httpClient = new HttpClient();
+            string content = (await httpClient.GetStringAsync(url));
             return content;
         }
 
-        public void ResolvePersonLocation(string content)
+        public void ResolvePersonLocation(string content, Person person)
         {
-            XmlDocument document = new XmlDocument();
-            document.LoadXml(content);
+            XDocument xDoc = XDocument.Parse(content);
+            var status = (from s in xDoc.Elements("GeocoderSearchResponse").Elements("status")
+                          select s.Value).FirstOrDefault();
 
+            if (!string.IsNullOrEmpty(status))
+            {
+                int statusInt = Convert.ToInt32(status);
+                if (statusInt == 0)
+                {
+                    var lat = (from a in xDoc.Elements("GeocoderSearchResponse")
+                                   .Elements("result")
+                                   .Elements("location")
+                                   .Elements("lat")
+                               select a.Value).FirstOrDefault();
+                    var lnt = (from a in xDoc.Elements("GeocoderSearchResponse")
+                                   .Elements("result")
+                                   .Elements("location")
+                                   .Elements("lng")
+                               select a.Value).FirstOrDefault();
+                    person.Lat = Convert.ToDouble(lat);
+                    person.Lnt = Convert.ToDouble(lnt);
+                }
+                else
+                {
+                    Console.WriteLine("Get xml status error,status is {0}", status);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Get xml status error,status is null");
+            }
         }
     }
 }
--- CarMatrixDataReader/Program.cs
+++ CarMatrixDataReader/Program.cs
@@ -23,52 +23,47 @@ namespace CarMatrixDataReader
             //    container.SaveChanges();
             //}
 
-            //string mapKey = ConfigurationManager.AppSettings["map_key"];
-            //string url = "http://api.map.baidu.com/geocoder/v2/" + "?address=百度大厦&output=xml&ak=" + mapKey;
 
-            //AddressTrans at = new AddressTrans();
-            //string content = at.HttpRequest(url);
-            //Console.WriteLine("{0}", content);
+            
+
+            //string path = @"D:\temp.xlsx";
+            //FileOperator op = new FileOperator(path);
+            //DataSet ds = op.LoadDataFromExcel();
+            //if (ds != null && ds.Tables != null)
+            //{
+            //    foreach (DataTable dt in ds.Tables)
+            //    {
+            //        if (!dt.TableName.Equals("_xlnm#_FilterDatabase"))
+            //        {
+            //            int count = dt.Columns.Count;                         
+
+            //            foreach (DataRow row in dt.Rows)
+            //            {
+            //                for (int i = 0; i < count; i++)
+            //                {
+            //                    object obj = row[i];
+            //                    Console.WriteLine("Column index is {0}.object is {1}", i, obj.ToString());
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
 
 
-            string path = @"D:\temp.xlsx";
-            FileOperator op = new FileOperator(path);
-            DataSet ds = op.LoadDataFromExcel();
-            if (ds != null && ds.Tables != null)
-            {
-                foreach (DataTable dt in ds.Tables)
-                {
-                    if (!dt.TableName.Equals("_xlnm#_FilterDatabase"))
-                    {
-                        //List<string> collectionName = new List<string>();
-                        int count = dt.Columns.Count;
-                        //foreach (DataColumnCollection dcc in dt.Columns)
-                        //{
-                        //    int count = dcc.Count;
-                        //    for (int i = 0; i < count; i++)
-                        //    {
-                        //        DataColumn dc = dcc[i];
-                        //        collectionName.Add(dc.ColumnName);
-                        //    }
-                        //}
-                         
-
-                        foreach (DataRow row in dt.Rows)
-                        {
-                            //foreach (string name in collectionName)
-                            //{
-                            for (int i = 0; i < count; i++)
-                            {
-                                object obj = row[i];
-                                Console.WriteLine("Column index is {0}.object is {1}", i, obj.ToString());
-                            }
-                            //}
-                        }
-                    }
-                }
-            }
-
+            GetHttpContent();
+            Console.WriteLine("{0}", "end");
             Console.ReadKey();
+        }
+
+        static async void GetHttpContent()
+        {
+            string mapKey = ConfigurationManager.AppSettings["map_key"];
+            string url = "http://api.map.baidu.com/geocoder/v2/" + "?address=百度大厦&output=xml&ak=" + mapKey;
+
+            AddressTrans at = new AddressTrans();
+            string content = await at.Taskmethod(url);
+            at.ResolvePersonLocation(content);
+            Console.WriteLine("{0}", content);
         }
     }
 }
