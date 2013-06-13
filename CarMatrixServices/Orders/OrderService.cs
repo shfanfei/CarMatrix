26f59823745973625ea5844a636f72b854a1ab02
fanfei <shfanfei@live.cn>
2013-06-13 18:53:03 +0800

    add order service and test code

---
+++ CarMatrixServices/Orders/OrderService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarMatrixCore.DAL;
+using CarMatrixData.Models;
+
+namespace CarMatrixServices.Orders
+{
+    public class OrderService
+    {
+        private IRepository<Order> orderRepository;
+        private readonly ModelsContainer container;
+
+        public OrderService()
+        { 
+            container = new ModelsContainer();
+            orderRepository = new Repository<Order>(container);
+        }
+
+        public IEnumerable<string> GetOrdersYears()
+        {
+            var year = from y in orderRepository.GetEntities
+                       select y.BuyTime.ToShortDateString();
+            return year.Distinct();
+        }
+    }
+}
